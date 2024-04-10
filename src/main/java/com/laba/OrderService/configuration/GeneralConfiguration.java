cd1878551b4e288e99c5630f9117986b64c2f4c7
nasan <nesrin.asan@hepsiburada.com>
2024-04-10 22:59:57 +0300

    ders 16

--- src/main/java/com/laba/OrderService/configuration/GeneralConfiguration.java
+++ src/main/java/com/laba/OrderService/configuration/GeneralConfiguration.java
@@ -3,6 +3,7 @@ package com.laba.OrderService.configuration;
 
 import feign.Logger;
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableFeignClients
+@EnableDiscoveryClient
 public class GeneralConfiguration {
 
 
